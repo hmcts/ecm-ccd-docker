8b39b5a873dd577087ecad6afd4d6d8d13398001
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2022-05-05 14:49:39 +0100

    Replace IdAM stub with IdAM simulator

--- bin/ecm/init-ecm.sh
+++ bin/ecm/init-ecm.sh
@@ -3,4 +3,21 @@
 set -eu
 dir=$(dirname ${0})
 
+${dir}/../add-users.sh
+
 ${dir}/../add-ccd-roles.sh
+
+if [[ ${ENGLANDWALES_CCD_CONFIG_PATH} ]]
+then
+  ${dir}/import-ccd-config.sh e
+fi
+
+if [[ ${SCOTLAND_CCD_CONFIG_PATH} ]]
+then
+  ${dir}/import-ccd-config.sh s
+fi
+
+if [[ ${ADMIN_CCD_CONFIG_PATH} ]]
+then
+  ${dir}/import-ccd-config.sh a
+fi
